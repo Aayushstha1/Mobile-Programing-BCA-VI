c141f5548b6773acc308d87cacc9040688396b15
Aayushstha1 <sudipk339@gmail.com>
2026-02-11 08:05:46 +0545

    New

--- GoogleMapExample/app/src/main/java/com/example/googlemapexample/MyActivity.java
+++ GoogleMapExample/app/src/main/java/com/example/googlemapexample/MyActivity.java
@@ -3,42 +3,86 @@ package com.example.googlemapexample;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public abstract class MyActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MyActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    MapView mapView;
+    private MapView mapView;
+    private GoogleMap gMap;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_activity_layout);
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        MapsInitializer.initialize(getApplicationContext());
+
         mapView.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+
         LatLng location = new LatLng(26.6511244, 87.2726496);
 
-        googleMap.addMarker(
-                new MarkerOptions()
-                        .position(location)
-                        .title("INC")
-        );
+        gMap.addMarker(new MarkerOptions()
+                .position(location)
+                .title("INC"));
 
-        googleMap.moveCamera(
-                CameraUpdateFactory.newLatLngZoom(location, 16.0f)
-        );
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16.0f));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
